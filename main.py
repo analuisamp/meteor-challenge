9faeee9c13b95b53e1fb9cbe5bc2cc759c9badad
analuisamp <dev.06@controlit.com.br>
2024-05-13 17:01:04 -0300

    Meteors falling into the water

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 import cv2
 import numpy as np
 
+#Carregando imagem
 img = cv2.imread("meteor_challenge_01.png")
 
 if img is None:
     print("Não foi possível carregar a imagem.")
     exit()
 
-#Convertendo a imagem para escala de cinza
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Adicionando a cor das estrelas
+mask_white = cv2.inRange(img, (255, 255, 255), (255, 255, 255))
 
-#Definindo uma faixa de cor branca para a detecção de estrelas
-#Corrindo problema de quando aproximava a imagem e pela perda de qualidade, não identificava a cor da mesma maneira
-lower_white = np.array([200, 200, 200])
-upper_white = np.array([255, 255, 255])
+#Adicionando a cor dos meteoros
+mask_red = cv2.inRange(img, (0, 0, 255), (0, 0, 255))
 
-#Threshold para encontrar as estrelas
-_, stars_binary = cv2.threshold(gray_img, 220, 255, cv2.THRESH_BINARY)
-
-# Definindo a faixa de cor vermelha para a detecção de meteoros
-lower_red = np.array([0, 0, 150])
-upper_red = np.array([80, 80, 255])
-
-#Meteoros
-mask_red = cv2.inRange(img, lower_red, upper_red)
-
-#Estrelas
-mask_white = cv2.inRange(img, lower_white, upper_white)
-
+#Contando os pixels de cada cor
 stars_count = cv2.countNonZero(mask_white)
 meteors_count = cv2.countNonZero(mask_red)
 
 print("Número de Estrelas:", stars_count)
 print("Número de Meteoros:", meteors_count)
+
+#Adicionando a cor da água
+mask_blue = cv2.inRange(img, (255, 0, 0), (255, 0, 0))
+
+meteors_in_water = 0
+
+#Percorre todas as colunas da imagem
+#Caso seja verificado que há pixels azuis e pixels vermelhos na respectiva coluna, deve-se contabilizar a quant de pixels vermelhos da coluna
+for x in range(img.shape[1]):  
+    blue_found = False 
+    red_found = False  
+    
+    #Verifica se há um pixel azul na coluna
+    if np.any(mask_blue[:, x] != 0):
+        blue_found = True
+
+    #Verifica se há um pixel vermelho na coluna
+    if np.any(mask_red[:, x] != 0):
+        red_found = True
+
+    #Caso ambas as condições sejam verdadeiras, adicionamos os meteoros à contagem
+    if blue_found and red_found:
+        meteors_per_column = cv2.countNonZero(mask_red[:, x])  
+        #Adicionando o número de meteoros da coluna na água
+        meteors_in_water += meteors_per_column  
+
+print("Meteoros caindo na água:", meteors_in_water)
